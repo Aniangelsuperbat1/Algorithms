972b55460326e7a4cb24bbae2f3bab903c864df6
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-11-19 16:39:39 -0500

    initial commit.

---
+++ Linear Search/example.py
@@ -0,0 +1,13 @@
+# Linear search: Searches through a set of data to see if target element is in data set.
+
+# O(N) time
+
+Data: (1, 3, 5, 67, 12, 54, 75, 342)
+Target: 54
+
+
+def Linear_Search(Data, target):
+    for num in range(len(data)):
+        if Data[num] == (Target):
+            Return True
+    Return False
