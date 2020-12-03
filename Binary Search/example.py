6ee7730ad4dfa690430e3a469ab50c0e6a3917f8
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-03 13:45:01 -0500

    Added notes.

--- Binary Search/example.py
+++ Binary Search/example.py
@@ -7,7 +7,6 @@ def binary_search(sequence, item):
     start_index = 0
     end_index = len(sequence) - 1
 
-
     while start_index <= end_index:
         midpoint = start_index + (end_index - start_index) // 2
         midpoint_value = sequence[midpoint]
--- Stacks/example.py
+++ Stacks/example.py
@@ -1,3 +1,13 @@
+# Stacks:
+# Last in, first out. LIFO
+# Append(): puts an item on top of stack
+# pop(): takes an item off of stack
+# peek(): lets you see an item on top of stack without removing it
+# clear(): clears all items from stack
+# Examples/use cases:
+# Undo Command: track which commands have been executed. Pop last command off command stack to undo it.
+
+
 class Stack:
     def __init__(self):
         self._data = []
@@ -10,6 +20,7 @@ class Stack:
 
     def peek(self):
         return self._data[len(self._data) - 1]
+
 
 stack = Stack()
 stack.push(5)
