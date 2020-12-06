37847f96b3494c017ac7f6ea058737326db13407
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-05 21:22:36 -0500

    Added sets.

---
+++ Hash tables (Dictionaries)/Sets.py
@@ -0,0 +1,19 @@
+# Do not maintain insertion order
+
+# colors = {"red", "orange", "Yellow", "Green", "Blue", "Indigo", "Violet"}
+
+# combine two sets
+fave_1 = {"help", "love", "jump", "number"}
+fave_2 = {"lost", "number", "self", "love"}
+
+# all_fave = fave_1 | fave_2
+
+# # subtracting same elements from eat set
+# # Difference
+# only_fave = fave_1 - fave_2
+
+# Symmetric Difference
+# 
+SymDif = fave_1 ^ fave_2
+print(SymDif)
+
