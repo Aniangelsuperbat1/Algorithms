c3a42928704bba76a3a180f277c08557d791fddb
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-12-05 20:52:07 -0500

    Added notes.

---
+++ Hash tables (Dictionaries)/Dictionaries.py
@@ -0,0 +1,10 @@
+# # creates an ordered Dict
+# from collections import OrderedDict
+
+# OD = OrderedDict()
+
+# Dictionaries are not ordered and there are no Duplicate keys
+# Duplicate values are fine as long as keys are different
+
+
+
